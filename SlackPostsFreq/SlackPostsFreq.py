a0b0b8ab4c4c02a079120740318c4aa79c2373a6
Adam <adam.pavlat@gmail.com>
2017-03-27 22:32:43 +0200

    sdb

--- SlackPostsFreq/SlackPostsFreq.py
+++ SlackPostsFreq/SlackPostsFreq.py
@@ -12,9 +12,13 @@ def utimeToReadebleTime(utime):
 utimeNow = time.time()
 readebleTime  = utimeToReadebleTime(utimeNow)
 
+#load token
+tokf = read('', 'r')
+token = tokf.read()
+
 #get channels.history
 print('Loading history in ' + readebleTime)
-ret = urllib.request.urlopen('https://slack.com/api/channels.history?token=xoxp-18038795252-135432840195-161044726919-3e573edec59f92ad8420c224ae389006&channel=C0JHTES4B&pretty=1')
+ret = urllib.request.urlopen('https://slack.com/api/channels.history?token=' + token + '&channel=C0JHTES4B&pretty=1')
 
 #nice print json
 #print(json.dumps(json.load(ret), indent=4, sort_keys=True))
