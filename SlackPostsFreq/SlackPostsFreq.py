913e97b512852e4eca938909f68b125a41666307
SupermanAdam007 <adam.pavlat@gmail.com>
2017-03-27 20:20:56 +0200

    time feature added

--- SlackPostsFreq/SlackPostsFreq.py
+++ SlackPostsFreq/SlackPostsFreq.py
@@ -5,14 +5,16 @@ import urllib.request
 import time
 import datetime
 
+def utimeToReadebleTime(utime):
+    return datetime.datetime.fromtimestamp(utime).strftime('%Y-%m-%d %H:%M:%S')
 
 # current UNIX time 
 utimeNow = time.time()
-readebleTime  = datetime.datetime.fromtimestamp(utimeNow).strftime('%Y-%m-%d %H:%M:%S')
+readebleTime  = utimeToReadebleTime(utimeNow)
 
 #get channels.history
 print('Loading history in ' + readebleTime)
-ret = urllib.request.urlopen('https://slack.com/api/channels.history?token=xoxp-18038795252-135432840195-159583001056-5633a19bc6f7a1294b4b2d82cd591082&channel=C0JHTES4B&pretty=1')
+ret = urllib.request.urlopen('https://slack.com/api/channels.history?token=xoxp-18038795252-135432840195-161044726919-3e573edec59f92ad8420c224ae389006&channel=C0JHTES4B&pretty=1')
 
 #nice print json
 #print(json.dumps(json.load(ret), indent=4, sort_keys=True))
@@ -22,5 +24,9 @@ jsonRet = json.load(ret)
 if jsonRet['ok'] == True:
     print('Successfully loaded history')
     #continue (az to bude ve smycce)
+else:
+    print('Not successfully loaded history')
+    exit()
 
-
+for x in jsonRet['messages']:
+    print(utimeToReadebleTime(float(x['ts'])))
